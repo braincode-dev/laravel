6c338dfc9f3da10a1c0b61f523cc4f3c3a6664b3
braincode-dev <50422463+braincode-dev@users.noreply.github.com>
2019-11-20 22:09:51 +0200

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -35,3 +35,52 @@ $(".all_companies_home").on("scroll", function(){
 
                     }
                 })
+
+// Scroll in typing input
+$(document).on('keyup', '.chosen-search-input', function(){
+
+
+                    // let search = $('.chosen-search-input').val();
+                    let search = $(this).val();
+                    let data_type = $(".data_type").val();
+                    let that = $(this).closest('form');
+                    // console.log(that);
+                    $.ajax(
+                        {
+                            url: "/construct",
+                            type: 'post',
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            data: {
+                                search: search,
+                                data_type: data_type
+                            },
+                            success: function (result) {
+                                let res = result.html;
+
+                                that.find('.chosen-results').html(res);
+
+                                // console.log(result.option);
+                                $('.modal').on('click', '.get_list_comp', function(e){
+                                    var companyId = $(this).data('option-array-index');
+                                    var companyName = $(this).text();
+                                    // console.log(companyId);
+                                    let thaten = $(this).closest('form');
+
+                                    if (companyId && companyName) {
+                                        let newOption = new Option(companyName, companyId, true, true);
+                                        // Append it to the select
+                                        thaten.find('.add-comment-form__company').append(newOption).trigger("chosen:updated");
+                                        // $('#add-comment-form__company').closest('form').trigger('change');chosen:updated
+
+                                    }
+
+                                });
+                                // $('.chosen-select').trigger('change');
+                            },
+                            error: function (error) {
+                                // console.log(error);
+                            }
+                        });
+                });
